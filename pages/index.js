4d8a887c67b66b740e1c06e5ad3e7a5306e04062
Gilooooo <118784503+Gilooooo@users.noreply.github.com>
2023-02-25 10:56:42 +0800

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="main">
+        <div className="container">
+          <img src="image-qr-code.png" />
+          <div>
+            <h3>Improve your front-end skills by building projects  </h3>
+            <p>
+              Scan the QR code to visit Frontend Mentor and take your coding
+              skills to the next level
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
